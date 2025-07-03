ca4300e8e9ede9995448a8623b01a47e70fd6f88
Mohamed Derkaoui <mderkaoui@dawan.fr>
2025-07-03 10:12:35 +0200

    Launch task manually

--- src/main/java/fr/dawan/project1/controllers/CategoryController.java
+++ src/main/java/fr/dawan/project1/controllers/CategoryController.java
@@ -10,27 +10,35 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.print.attribute.standard.Media;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/v1/categories")
 public class CategoryController extends GenericController<CategoryDto> {
 
     private CategoryService categoryService;
-
+    private TaskScheduler taskScheduler;
 
 
     @Autowired
-    public CategoryController(CategoryService categoryService) {
+    public CategoryController(CategoryService categoryService, TaskScheduler taskScheduler) {
         super(categoryService);
         this.categoryService=categoryService;
+        this.taskScheduler = taskScheduler;
     }
 
     @GetMapping(value={"/{page}/{size}/{search}"}, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -60,6 +68,23 @@ public class CategoryController extends GenericController<CategoryDto> {
                 .body(categoryService.saveOrUpdateWithImage(categoryStr,file));
 
 
+    }
+
+
+    @GetMapping(value="/task", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String launchTask(){
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    categoryService.count(); // TO REPLACE WITH YOUR TASK
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        taskScheduler.scheduleAtFixedRate(runnable, Instant.now(), Duration.of(10, ChronoUnit.SECONDS));
+        return "task launched...";
     }
 
 
